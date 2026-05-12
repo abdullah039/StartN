1d0ced65897c04797d918a792dd0d554ee56d884
Abdullah Maqsood <abdullahmaqsood039@gmail.com>
2026-05-12 19:43:40 +0500

    initial code upload

---
+++ PushCode Start.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"My name is Abdullah Maqsood"<<endl;
+    cout<<"Reg.No: 2025-CS-554"<<endl;
+    cout<<"I learn how to push code on GitHub";
+}
