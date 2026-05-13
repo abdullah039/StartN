9b3e27c452433ed6a86eac1d55e6f2306fd9f8a7
Abdullah Maqsood <abdullahmaqsood039@gmail.com>
2026-05-13 18:59:17 +0500

    Tense change

--- PushCode Start.cpp
+++ PushCode Start.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 int main(){
     cout<<"My name is Abdullah"<<endl;
-    cout<<"I Studied in UET"<<endl;
+    cout<<"I am Studing in UET"<<endl;
     cout<<"I learn how to push code on GitHub";
 }
+
